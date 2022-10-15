a1846405fafb34a0b3a4e9b5f5f3e8654db14283
HOWARD HUANG <howardhuang@college.harvard.edu>
2022-10-15 16:46:09 -0400

    updated api

--- check_grammar.py
+++ check_grammar.py
@@ -4,7 +4,7 @@ import requests
 
 
 def check_grammar(input_str, language='en_US'):
-    tool = language_tool_python.LanguageTool('en_US')
+    tool = language_tool_python.LanguageToolPublicAPI(language)
     matches = tool.check(input_str)
     is_bad_rule = lambda rule: rule.message == 'Possible spelling mistake found.' and len(rule.replacements) and rule.replacements[0][0].isupper()
     matches = [rule for rule in matches if not is_bad_rule(rule)]
