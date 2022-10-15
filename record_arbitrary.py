d0fb10779cc11698255bc695826b6d886b56def5
HOWARD HUANG <howardhuang@college.harvard.edu>
2022-10-15 15:50:01 -0400

    interface updates

--- record_arbitrary.py
+++ record_arbitrary.py
@@ -2,6 +2,10 @@ import argparse
 import tempfile
 import queue
 import sys
+import sounddevice as sd
+import soundfile as sf
+import numpy  # Make sure NumPy is loaded before it is used in the callback
+assert numpy  # avoid "imported but unused" message (W0611)
 
 
 def int_or_str(text):
@@ -11,30 +15,27 @@ def int_or_str(text):
     except ValueError:
         return text
 
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-parser.add_argument(
-    '-d', '--device', type=int_or_str,
-    help='input device (numeric ID or substring)')
-parser.add_argument(
-    '-r', '--samplerate', type=int, help='sampling rate')
-parser.add_argument(
-    '-c', '--channels', type=int, default=1, help='number of input channels')
-parser.add_argument(
-    'filename', nargs='?', metavar='FILENAME',
-    help='audio file to store recording to')
-parser.add_argument(
-    '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
-args = parser.parse_args()
+def create_parser():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '-l', '--list-devices', action='store_true',
+        help='show list of audio devices and exit')
+    parser.add_argument(
+        '-d', '--device', type=int_or_str,
+        help='input device (numeric ID or substring)')
+    parser.add_argument(
+        '-r', '--samplerate', type=int, help='sampling rate')
+    parser.add_argument(
+        '-c', '--channels', type=int, default=1, help='number of input channels')
+    parser.add_argument(
+        'filename', nargs='?', metavar='FILENAME',
+        help='audio file to store recording to')
+    parser.add_argument(
+        '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
+    
+    return parser
 
-try:
-    import sounddevice as sd
-    import soundfile as sf
-    import numpy  # Make sure NumPy is loaded before it is used in the callback
-    assert numpy  # avoid "imported but unused" message (W0611)
-
+def setup_record(parser, args):
     if args.list_devices:
         print(sd.query_devices())
         parser.exit(0)
@@ -46,24 +47,10 @@ try:
         args.filename = tempfile.mktemp(prefix='delme_rec_unlimited_',
                                         suffix='.wav', dir='')
     q = queue.Queue()
+    return q
 
-    def callback(indata, frames, time, status):
-        """This is called (from a separate thread) for each audio block."""
-        if status:
-            print(status, file=sys.stderr)
-        q.put(indata.copy())
-
-    # Make sure the file is opened before recording anything:
-    with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
-                      channels=args.channels, subtype=args.subtype) as file:
-        with sd.InputStream(samplerate=args.samplerate, device=args.device,
-                            channels=args.channels, callback=callback):
-            print('press Ctrl+C to stop the recording')
-            while True:
-                file.write(q.get())
-
-except KeyboardInterrupt:
-    print('\nRecording finished: ' + repr(args.filename))
-    parser.exit(0)
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
+def callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    q.put(indata.copy())
--- user_interface.py
+++ user_interface.py
@@ -1,18 +1,38 @@
 from sys import breakpointhook
 import PySimpleGUI as sg
+import argparse
+import tempfile
+import queue
+import sys
+import sounddevice as sd
+import soundfile as sf
+import numpy  # Make sure NumPy is loaded before it is used in the callback
+assert numpy  # avoid "imported but unused" message (W0611)
+from record_arbitrary import *
 
 def recording():
     layout = [[sg.Text("Recording...")], [sg.Text("Click to stop")], [sg.Button("STOP")]]
 
     window = sg.Window("SpeechLearn", layout, margins = (300,200))
 
-    while True:
-        event, values = window.read()
-        if event in (None, 'Exit'):
-            break
+    parser = create_parser()
+    args = parser.parse_args()
+    q = setup_record(parser, args)
 
-        if event == "STOP":
-            break
+    with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
+                      channels=args.channels, subtype=args.subtype) as file:
+        with sd.InputStream(samplerate=args.samplerate, device=args.device,
+                            channels=args.channels, callback=callback):
+            while True:
+                file.write(q.get())
+                event, values = window.read()
+                if event in (None, 'Exit'):
+                    parser.exit(0)
+                    break
+
+                if event == "STOP":
+                    parser.exit(0)
+                    break
     window.close()
     return
 
