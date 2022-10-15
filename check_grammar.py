34e85fe2a06845e38ec97197cb49a3dce2de91a4
HOWARD HUANG <howardhuang@college.harvard.edu>
2022-10-14 21:50:40 -0400

    eidts

--- check_grammar.py
+++ check_grammar.py
@@ -1,3 +1,15 @@
+from gramformer import Gramformer
+import torch
+
+def initialize_tool(language='en_US'):
+    if not internet_on():
+        tool = language_tool_python.LanguageTool(language)
+    else:
+        tool = language_tool_python.LanguageToolPublicAPI(language)
+    return tool
+
+
+
 import language_tool_python
 import requests
 
@@ -21,9 +33,7 @@ def analyze_string(tool, input_str):
 def correct_string(matches, input_str):
     is_bad_rule = lambda rule: rule.message == 'Possible spelling mistake found.' and len(rule.replacements) and rule.replacements[0][0].isupper()
     matches = [rule for rule in matches if not is_bad_rule(rule)]
-    return language_tool_python.utils.correct(input_str, matches)
-
-
+    return language_tool_python.utils.correct(input_str, matches) '''
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 # main control flow
 from check_grammar import *
-from record_speech import *
-from speech_to_text import *
+#from record_speech import *
+#from speech_to_text import *
+
+
+
+#test grammar
+input_str = input("here: ")
+
+tool = initialize_tool("en-US")
+result = analyze_string(tool, input_str)
+str = correct_string(result, input_str)
+print(str)
+input_str = str
+result = analyze_string(tool, input_str)
+str = correct_string(result, input_str)
+print(str)
+input_str = str
+result = analyze_string(tool, input_str)
+str = correct_string(result, input_str)
+print(str)
+input_str = str
+result = analyze_string(tool, input_str)
+str = correct_string(result, input_str)
+print(str)
+input_str = str
+result = analyze_string(tool, input_str)
+str = correct_string(result, input_str)
+print(str)
